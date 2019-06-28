90c9a3ead4c63c94b6e16f189fe0d51a839d2728
CristobalSalazar <chris.salza@gmail.com>
2019-06-28 18:03:21 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/style.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Reactron</h1>
+    </div>
+  );
+}
+
+export default App;
